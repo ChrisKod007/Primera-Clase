d7944d0e9d3517aae04483a0165d8143530c5996
ChrisKod007 <cdbm001@gmail.com>
2024-08-31 15:17:31 -0500

    Update ejercicio4.py
    cambie el xd  por equis de

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,7 +1,7 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso"
-            "XD" : "Es como un emoticon la X son los ojos cerrados, y la D es una carcajada",
+            "equis de" : "Es como un emoticon la X son los ojos cerrados, y la D es una carcajada",
             }
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
